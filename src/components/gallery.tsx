1f577a4788a7ced3e2b6fe2a1551840769fc48dd
Yoonhyoung-Lee <54613623+Yoonhyoung-Lee@users.noreply.github.com>
2023-12-10 19:59:28 +0900

    feat: add gif, meme images

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -1,37 +1,92 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { CardHeader, CardContent, Card } from "@/components/ui/card";
-import { mediaKitImages } from "@/data/images";
+import { gifImages, mediaKitImages, memeImages } from "@/data/images";
+import { For } from "million/react";
 import Image from "next/image";
+import { useState } from "react";
 
 export function Gallery() {
+  const [view, setView] = useState<"Media Kit" | "GIFs" | "Memes">("Memes");
+
   return (
     <div className="p-6">
       <header className="flex justify-between items-center mb-6">
         <h1 className="text-4xl font-bold">Monad Gallery</h1>
         <div className="flex gap-4">
-          <Button variant="outline">Media Kit</Button>
-          <Button variant="outline">Stickers</Button>
-          <Button variant="outline">GIFs</Button>
-          <Button variant="outline">Memes</Button>
+          <Button variant="outline" onClick={() => setView("Media Kit")}>
+            Media Kit
+          </Button>
+          <Button variant="outline" onClick={() => setView("GIFs")}>
+            GIFs
+          </Button>
+          <Button variant="outline" onClick={() => setView("Memes")}>
+            Memes
+          </Button>
         </div>
       </header>
       <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
-        {mediaKitImages.map((item) => (
-          <Card key={item.alt}>
-            <CardHeader>
-              <h2 className="text-xl font-bold">{item.alt}</h2>
-            </CardHeader>
-            <CardContent>
-              <Image
-                alt={item.alt}
-                className="w-full h-full object-cover rounded-lg overflow-hidden"
-                height="500"
-                src={item.src}
-                width="500"
-              />
-            </CardContent>
-          </Card>
-        ))}
+        {view === "Media Kit" && (
+          <For each={mediaKitImages}>
+            {(item) => (
+              <Card key={item.alt}>
+                <CardHeader>
+                  <h2 className="text-xl font-bold">{item.alt}</h2>
+                </CardHeader>
+                <CardContent>
+                  <Image
+                    alt={item.alt}
+                    className="w-full h-full object-cover rounded-lg overflow-hidden"
+                    height="500"
+                    src={item.src}
+                    width="500"
+                  />
+                </CardContent>
+              </Card>
+            )}
+          </For>
+        )}
+        {view === "GIFs" && (
+          <For each={gifImages}>
+            {(item) => (
+              <Card key={item.alt}>
+                <CardHeader>
+                  <h2 className="text-xl font-bold">{item.alt}</h2>
+                </CardHeader>
+                <CardContent>
+                  <Image
+                    alt={item.alt}
+                    className="w-full h-full object-cover rounded-lg overflow-hidden"
+                    height="500"
+                    src={item.src}
+                    width="500"
+                  />
+                </CardContent>
+              </Card>
+            )}
+          </For>
+        )}
+        {view === "Memes" && (
+          <For each={memeImages}>
+            {(item) => (
+              <Card key={item.alt}>
+                <CardHeader>
+                  <h2 className="text-xl font-bold">{item.alt}</h2>
+                </CardHeader>
+                <CardContent>
+                  <Image
+                    alt={item.alt}
+                    className="w-full h-full object-cover rounded-lg overflow-hidden"
+                    height="500"
+                    src={item.src}
+                    width="500"
+                  />
+                </CardContent>
+              </Card>
+            )}
+          </For>
+        )}
       </div>
     </div>
   );
--- src/data/images.ts
+++ src/data/images.ts
@@ -57,3 +57,213 @@ export const mediaKitImages: Image[] = [
     alt: "monad_black",
   },
 ];
+
+export const gifImages: Image[] = [
+  {
+    src: "/gif/pepe.gif",
+    alt: "pepe",
+  },
+  {
+    src: "/gif/ezgif-2-7039a9e1b5.gif",
+    alt: "ezgif-2-7039a9e1b5",
+  },
+  {
+    src: "/gif/1moon.gif",
+    alt: "1moon",
+  },
+  {
+    src: "/gif/48-unscreen.gif",
+    alt: "48-unscreen",
+  },
+  {
+    src: "/gif/ezgif-2-6dc3c2e0a7.gif",
+    alt: "ezgif-2-6dc3c2e0a7",
+  },
+  {
+    src: "/gif/ezgif-2-e407719032.gif",
+    alt: "ezgif-2-e407719032",
+  },
+  {
+    src: "/gif/5.gif",
+    alt: "5",
+  },
+  {
+    src: "/gif/11.gif",
+    alt: "11",
+  },
+  {
+    src: "/gif/4.gif",
+    alt: "4",
+  },
+  {
+    src: "/gif/600.gif",
+    alt: "600",
+  },
+  {
+    src: "/gif/walk.gif",
+    alt: "walk",
+  },
+];
+
+export const memeImages: Image[] = [
+  {
+    src: "/meme/week10-1.jpeg",
+    alt: "week10-1",
+  },
+  {
+    src: "/meme/week10-2.jpeg",
+    alt: "week10-2",
+  },
+  {
+    src: "/meme/week10-3.jpeg",
+    alt: "week10-3",
+  },
+  {
+    src: "/meme/week10-4.jpeg",
+    alt: "week10-4",
+  },
+  {
+    src: "/meme/week9-1.jpeg",
+    alt: "week9-1",
+  },
+  {
+    src: "/meme/week9-2.jpeg",
+    alt: "week9-2",
+  },
+  {
+    src: "/meme/week9-3.jpeg",
+    alt: "week9-3",
+  },
+  {
+    src: "/meme/week9-4.jpeg",
+    alt: "week9-4",
+  },
+  {
+    src: "/meme/week8-1.jpeg",
+    alt: "week8-1",
+  },
+  {
+    src: "/meme/week8-2.jpeg",
+    alt: "week8-2",
+  },
+  {
+    src: "/meme/week8-3.jpeg",
+    alt: "week8-3",
+  },
+  {
+    src: "/meme/week8-4.jpeg",
+    alt: "week8-4",
+  },
+  {
+    src: "/meme/week7-1.jpeg",
+    alt: "week7-1",
+  },
+  {
+    src: "/meme/week7-2.jpeg",
+    alt: "week7-2",
+  },
+  {
+    src: "/meme/week7-3.jpeg",
+    alt: "week7-3",
+  },
+  {
+    src: "/meme/week7-4.jpeg",
+    alt: "week7-4",
+  },
+  {
+    src: "/meme/week6-1.jpeg",
+    alt: "week6-1",
+  },
+  {
+    src: "/meme/week6-2.jpeg",
+    alt: "week6-2",
+  },
+  {
+    src: "/meme/week6-3.jpeg",
+    alt: "week6-3",
+  },
+  {
+    src: "/meme/week6-4.jpeg",
+    alt: "week6-4",
+  },
+  {
+    src: "/meme/week5-1.jpeg",
+    alt: "week5-1",
+  },
+  {
+    src: "/meme/week5-2.jpeg",
+    alt: "week5-2",
+  },
+  {
+    src: "/meme/week5-3.jpeg",
+    alt: "week5-3",
+  },
+  {
+    src: "/meme/week5-4.jpeg",
+    alt: "week5-4",
+  },
+  {
+    src: "/meme/week4-1.jpeg",
+    alt: "week4-1",
+  },
+  {
+    src: "/meme/week4-2.jpeg",
+    alt: "week4-2",
+  },
+  {
+    src: "/meme/week4-3.jpeg",
+    alt: "week4-3",
+  },
+  {
+    src: "/meme/week4-4.jpeg",
+    alt: "week4-4",
+  },
+  {
+    src: "/meme/week3-1.jpeg",
+    alt: "week3-1",
+  },
+  {
+    src: "/meme/week3-2.jpeg",
+    alt: "week3-2",
+  },
+  {
+    src: "/meme/week3-3.jpeg",
+    alt: "week3-3",
+  },
+  {
+    src: "/meme/week3-4.jpeg",
+    alt: "week3-4",
+  },
+  {
+    src: "/meme/week2-1.jpeg",
+    alt: "week2-1",
+  },
+  {
+    src: "/meme/week2-2.jpeg",
+    alt: "week2-2",
+  },
+  {
+    src: "/meme/week2-3.jpeg",
+    alt: "week2-3",
+  },
+  {
+    src: "/meme/week2-4.jpeg",
+    alt: "week2-4",
+  },
+  {
+    src: "/meme/week1-1.jpeg",
+    alt: "week1-1",
+  },
+  {
+    src: "/meme/week1-2.jpeg",
+    alt: "week1-2",
+  },
+  {
+    src: "/meme/week1-3.jpeg",
+    alt: "week1-3",
+  },
+  {
+    src: "/meme/week1-4.jpeg",
+    alt: "week1-4",
+  },
+];
